c350c8f03e473b5b582936c134d0b50ddd77acd4
Gaonkar-Narasimha <narasimhagaonkar987@gmail.com>
2022-12-08 11:57:45 -0500

    Initial Repo commit

---
+++ constants.py
@@ -0,0 +1,64 @@
+NORMALIZED_DATABASE_FILENAME = 'normalized.db'
+DATA_FILENAME = 'survey.csv'
+DELEMETER = ','
+
+GENDER_CREATE_TABLE_SQL = """CREATE TABLE IF NOT EXISTS [Gender] (
+            [Gender] TEXT NOT NULL,
+            [GenderCode] INTEGER NOT NULL
+        );
+        """
+GENDER_INSERT_TABLE = '''INSERT INTO Gender (Gender, GenderCode) VALUES(?,?)'''
+
+
+Gender = {
+    "A little about you":	2,
+    "Agender":	2,
+    "All":	2,
+    "Androgyne":	1,
+    "Cis Female":	1,
+    "Cis Male":	0,
+    "Cis Man":	0,
+    "Enby":	2,
+    "F":	1,
+    "Femake":	1,
+    "Female ":	1,
+    "Female (cis)":	1,
+    "Female (trans)":	1,
+    "Female":	1,
+    "Genderqueer":	2,
+    "Guy (-ish) ^_^":	0,
+    "M":	0,
+    "Mail":	0,
+    "Make":	0,
+    "Mal":	0,
+    "Male ":	0,
+    "Male (CIS)":	0,
+    "Male":	0,
+    "Male-ish":	0,
+    "Malr":	0,
+    "Man":	0,
+    "Nah":	2,
+    "Neuter":	2,
+    "Trans woman":	2,
+    "Trans-female":	2,
+    "Woman":	1,
+    "cis male":	2,
+    "cis-female/femme":	1,
+    "f":	1,
+    "femail":	1,
+    "female":	1,
+    "fluid":	1,
+    "m":	0,
+    "maile":	0,
+    "male leaning androgynous":	0,
+    "male":	0,
+    "msle":	0,
+    "non-binary":	2,
+    "ostensibly male":	0,
+    "p":	2,
+    "queer":	2,
+    "queer/she/they":	1,
+    "something kinda male?":	0,
+    "woman":	1,
+    'ostensibly male, unsure what that really means':	0,
+}
---
+++ main.py
@@ -0,0 +1,39 @@
+import databaseMethods as dm
+import pandas as pd
+import constants as c
+
+
+#Global variable 
+normalized_database_filename = c.NORMALIZED_DATABASE_FILENAME
+data_filename = c.DATA_FILENAME
+
+
+# Create new database and  establish the connection
+def createDatabase():
+    return dm.create_connection(normalized_database_filename, True)
+
+# Get Database connection
+def getDatabaseConnection():
+    return dm.create_connection(normalized_database_filename)
+
+# Create Gender table
+def createTable():
+    create_gender_table()
+
+def create_gender_table():
+    # Read the data from the csv file and create a list of tuples 
+    # Insert the data into the table
+    df = pd.read_csv(data_filename)
+    uniqueGender = df['Gender'].unique().tolist()
+
+    genderList = [(x, c.Gender[x]) for x in uniqueGender]
+    genderList = sorted(genderList, key = lambda x:x[0])
+    conn_norm = getDatabaseConnection()
+    dm.create_table(conn_norm, c.GENDER_CREATE_TABLE_SQL)
+    dm.execute_many_sql_statement(c.GENDER_INSERT_TABLE,genderList,conn_norm)
+
+def main():
+    conn_norm = createDatabase()
+    createTable()
+    conn_norm.close()
+main()
